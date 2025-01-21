28af8cc60a380fc2ca15d032680d6ad0456bd62c
Tommy Nguyen <nguyenanhnhat123456@gmail.com>
2025-01-22 01:21:24 +0700

    fix: add selector

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Page from 'app/components/LayoutComponents/Page';
 import { RemoteProvider, useRemoteContext } from 'app/remoteContext';
+import { useSelector, Provider } from 'react-redux';
+import store from 'app/store';
 function AppWrapper(props) {
   return (
-    <RemoteProvider>
-      <App {...props} />
-    </RemoteProvider>
+    <Provider store={store}>
+      <RemoteProvider>
+        <App {...props} />
+      </RemoteProvider>
+    </Provider>
   );
 }
 
 function App(props) {
-  const { store } = useRemoteContext();
+  const { store, selectors } = useRemoteContext();
   console.log('🚀 ~ App ~ store:', store);
+  const shopDetail = useSelector(selectors.getShop);
+  console.log('🚀 ~ App ~ store:', shopDetail);
   return (
     <Page>
       <h1>Hello</h1>
